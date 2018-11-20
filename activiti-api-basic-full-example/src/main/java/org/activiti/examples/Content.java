6d4c68e52d73d4c3c2ef356ef8712f3e71726d90
ryandawsonuk <ryandawson@cantab.net>
2018-11-20 09:53:39 +0000

    use list for tags

--- activiti-api-basic-full-example/src/main/java/org/activiti/examples/Content.java
+++ activiti-api-basic-full-example/src/main/java/org/activiti/examples/Content.java
@@ -4,16 +4,24 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY,property = "@class")
 public class Content {
 
     private String body;
     private boolean approved;
+    private List<String> tags;
 
     @JsonCreator
-    public Content(@JsonProperty("body")String body, @JsonProperty("approved")boolean approved){
+    public Content(@JsonProperty("body")String body, @JsonProperty("approved")boolean approved, @JsonProperty("tags")List<String> tags){
         this.body = body;
         this.approved = approved;
+        this.tags = tags;
+        if(this.tags == null){
+            this.tags = new ArrayList<>();
+        }
     }
 
     public String getBody() {
@@ -32,11 +40,20 @@ public class Content {
         this.approved = approved;
     }
 
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
     @Override
     public String toString() {
         return "Content{" +
                 "body='" + body + '\'' +
                 ", approved=" + approved +
+                ", tags=" + tags +
                 '}';
     }
 }
